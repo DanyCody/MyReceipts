0ad8e8ae821836fc957eb899309b38e01967600a
DanyCody <geoff.roussat@gmail.com>
2020-08-25 22:13:58 +0200

    versio  uplink ok

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Bonjour le monde")
+import anvil.server
+
+
+anvil.server.connect("VAQPKXKNKSSD4HYOAGS2NZER-H3E7QJIUC62EZ43O")
+
+
+@anvil.server.callable
+def hello_world():
+    return 'Hello World!'
+
+anvil.server.wait_forever()
+
+
