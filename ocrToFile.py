0e3cdc52ac53d0e3ca9f88057a6f6d75d9cff541
DanyCody <geoff.roussat@gmail.com>
2020-09-01 10:05:50 +0200

    ocr3

--- ocrToFile.py
+++ ocrToFile.py
@@ -21,10 +21,11 @@ def ocr(image):
     # file_bytes = io.BytesIO(open_cv_image)
     # print("Compressed File Size In Bytes: " + str(len(file_bytes.read())))
 
-    #with open("img/Compressed_image_Car1.jpg", 'rb') as f:
-    with io.BytesIO(open_cv_image) as f:
+    #with io.BytesIO(image.tobytes()) as f:
+
+    with open("img/Compressed_image_Car1.jpg", 'rb') as f:
         result = requests.post(url_api,
-            files={"Compressed_image_Car1.jpg": f},
+            files={"img/Compressed_image_Car1.jpg": f},
             data={"apikey": "fb6f75e9b088957", "scale": "true", "isTable": "true"})
 
     result = result.content.decode()
--- testOcr.py
+++ testOcr.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 import ocrToFile
+import io
 
 
 """ Load image from path. Return a numpy array """
@@ -8,9 +9,15 @@ import ocrToFile
 #img = img.resize((1512,2016), Image.ANTIALIAS)
 #img = img.resize((756,1008), Image.ANTIALIAS)
 #img.save('img/Compressed_image_Car1.jpg', optimize=True, quality=95)
+#img = Image.open("img/image_Car1.jpg")
+#print("Original Image : " + str(len(img.tobytes())))
+#img = img.resize((504,378))
+#img.save('img/Compressed_image_Car1.jpg')
 img = Image.open("img/Compressed_image_Car1.jpg")
+#print("Resized Image : " + str(len(img.tobytes())))
+#tstimage = open("img/Compressed_image_Car1.jpg", 'rb')
+
 res = ocrToFile.ocr(img)
-# res = ocrToFile.ocr(img.get_bytes())
 print(res)
 
 
